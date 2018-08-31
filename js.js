280e4d15a7701f3f1cb155f6116370a525a341b4
王鼎恩 <nick9815@gmail.com>
2018-08-31 20:32:47 +0800

    update website

---
+++ js.js
@@ -0,0 +1,192 @@
+var selection_books = [{
+        name: "博客來選書",
+        e_name: "books"
+    },
+    {
+        name: "誠品選書",
+        e_name: "eslite"
+    }
+];
+var eslitedatas = [{
+        name: "搶救與殺戮: 軍醫的戰爭回憶錄",
+        img: "http://pic.eslite.com/Upload/Product/201805/m/636613146453676250.jpg",
+        author: "強．克斯鐵特爾",
+        publish_house: "時報文化",
+        price: 480,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/04/24"
+    },
+    {
+        name: "史達林: 從革命者到獨裁者",
+        img: "http://pic.eslite.com/Upload/Product/201804/m/636598964323527663.jpg",
+        author: "奧列格．賀列夫紐克",
+        publish_house: "左岸文化",
+        price: 700,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/05/03"
+    },
+    {
+        name: "困在大腦裡的人",
+        img: "http://pic.eslite.com/Upload/Product/201804/m/636597237609166250.jpg",
+        author: "安卓恩．歐文",
+        publish_house: "采實文化",
+        price: 450,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/04/26"
+    },
+    {
+        name: "我只想活著: 七歲女孩的敘利亞烽火日常",
+        img: "http://pic.eslite.com/Upload/Product/201804/m/636603285016433750.jpg",
+        author: "芭娜．阿拉貝得",
+        publish_house: "遠見天下文化",
+        price: 350,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/04/30"
+    },
+    {
+        name: "起源",
+        img: "http://pic.eslite.com/Upload/Product/201804/m/636602664902881250.jpg",
+        author: "丹．布朗",
+        publish_house: " 時報文化",
+        price: 460,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/05/08"
+    },
+    {
+        name: "蜜蜂與遠雷",
+        img: "http://pic.eslite.com/Upload/Product/201804/m/636590324016433750.jpg",
+        author: "恩田陸",
+        publish_house: " 圓神",
+        price: 499,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/05/01"
+    },
+    {
+        name: "82年生的金智英",
+        img: "http://pic.eslite.com/Upload/Product/201804/m/636602418452840000.jpg",
+        author: "趙南柱",
+        publish_house: " 漫遊者文化",
+        price: 310,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/05/03"
+    },
+    {
+        name: "驟雨之島",
+        img: "http://pic.eslite.com/Upload/Product/201804/m/636594864904022500.jpg",
+        author: "顧德莎",
+        publish_house: " 有鹿文化",
+        price: 350,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/05/04"
+    }
+];
+
+var booksdatas = [{
+        name: "出家",
+        img: "http://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/75/0010787583.jpg&v=5afa5439&w=140&h=140",
+        author: "張忌",
+        publish_house: "漫遊者文化",
+        price: 340,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/05/31"
+    },
+    {
+        name: "阿飄",
+        img: "http://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/78/0010787864.jpg&v=5b064dde&w=348&h=348",
+        author: " 上官鼎, 劉兆玄",
+        publish_house: "時報出版",
+        price: 460,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/06/05"
+    },
+    {
+        name: "謊言回憶錄",
+        img: "http://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/82/0010788286.jpg&v=5b04ee4d&w=348&h=348",
+        author: "米蘭達．道爾",
+        publish_house: "木馬文化",
+        price: 330,
+        is_discount: true,
+        discount: 0.79,
+        date: "22018/06/06"
+    },
+    {
+        name: "再見媽咪，再見幸福",
+        img: "http://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/81/0010788196.jpg&v=5b0cf372&w=348&h=348",
+        author: "蓋兒‧霍尼曼",
+        publish_house: "悅知文化",
+        price: 380,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/06/01"
+    },
+    {
+        name: "十三歲後，我不再是我",
+        img: "http://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/72/0010787294.jpg&v=5af41f6d&w=348&h=348",
+        author: "山本潤",
+        publish_house: "三采",
+        price: 340,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/06/01"
+    },
+    {
+        name: "性的解析",
+        img: "http://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/82/0010788291.jpg&v=5b06b050&w=348&h=348",
+        author: "威廉．亞伯, 芭芭拉．薩雅德",
+        publish_house: "大家出版",
+        price: 600,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/06/06"
+    },
+    {
+        name: "我的好朋友黑漆漆",
+        img: "http://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/60/0010786067.jpg&v=5ae1aa75&w=348&h=348",
+        author: "艾瑪．亞略特",
+        publish_house: "小天下",
+        price: 350,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/05/17"
+    },
+    {
+        name: "妖怪地圖",
+        img: "http://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/82/0010788289.jpg&v=5b06b04a&w=348&h=348",
+        author: "珊卓拉．勞倫絲",
+        publish_house: "大家出版",
+        price: 500,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/06/06"
+    },
+    {
+        name: "高績效心智",
+        img: "http://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/80/0010788013.jpg&v=5afd5ab4&w=348&h=348",
+        author: "莫頓‧韓森",
+        publish_house: "天下文化 ",
+        price: 450,
+        is_discount: true,
+        discount: 0.79,
+        date: "2018/05/31"
+    }
+];
+
+var vm = new Vue({
+    el: "#app",
+    data: {
+        mode_detail: false,
+        books: booksdatas,
+        eslite: eslitedatas,
+        sel_books: selection_books
+    }
+});
